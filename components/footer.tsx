29e219e49a9dd3bdea6596da3887f0d7598545f5
Josue Guevara <tv.e5150@gmail.com>
2024-08-20 00:42:18 -0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3">
         <div className="flex items-center gap-3">
           <p className="text-center">
-            Clone this template on{" "}
+            Clone this website on{" "}
             <Link
               className="px-1 underline underline-offset-2"
               href="https://github.com/nisabmohd/Docs-Stater-Template"
